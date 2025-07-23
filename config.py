cbd51fd3619a4fbd9eff078b5bdae5905fde1df2
Kwan Chun Hoe <chkwan@golive.com.my>
2025-07-23 12:15:40 +0800

    no more save data to data.json and modified to allow create new json for different worksheet

--- config.py
+++ config.py
@@ -1,60 +1,67 @@
 import json
-import time
-
-import gspread
-from google.oauth2.service_account import Credentials
 
 import components.customError as customException
+from components.getWorkbook import getWorkbook
 
 try:
     with open("json/config.json", "r") as file:
         config = json.load(file)
+
+        workbook = getWorkbook()
+        sheet = workbook.worksheet(config["worksheetName"])
+        config["sheetID"] = sheet._properties['sheetId']
+
+        records = sheet.get_all_values()
+        for record in records:
+            if "#" in record[0]:
+                pass
+            else:
+                header_idx = records.index(record)
+                config["headerIndex"] = header_idx
+                break
+    with open("json/config.json", "w") as file:
+        json.dump(config, file, indent=2)
 except FileNotFoundError as e:
     raise customException.missingConfigJSON from None
 
-try:
-    scopes = [
-        config['scopes']
-    ]
-    creds = Credentials.from_service_account_file("json/config.json", scopes=scopes)
 
-    client = gspread.authorize(creds)
-    sheet_credential = config["workbookID"]
-    workbook = client.open_by_key(sheet_credential)
+def write_column_rule(action, name):
+    headers = records[header_idx]  # your original header list
+    headers_json = {
+        header: {
+            "rule": "",
+            "param": ""
+        }
+        for header in headers
+    }
+    if action == "create":
+        with open(f"json/{name}.json", "x") as file:
+            json.dump(headers_json, file, indent=4)
+    else:
+        with open(f"json/{name}.json", "w") as file:
+            json.dump(headers_json, file, indent=4)
 
-    sheet = workbook.worksheet(config["worksheetName"])
-    config["sheetID"] = sheet._properties['sheetId']
-    with open("json/config.json", "w") as file:
-        json.dump(config, file, indent=2)
-except KeyError as e:
-    raise customException.missingField(e) from None
 
-records = sheet.get_all_values()
-headers = records[0]
-rows = records[1:]
-
-headers_json = dict.fromkeys(headers, "")
+while True:
+    try:
+        choice = int(input("\nAre you...\n"
+                           "1: working on a new worksheet? \n\t- A new json file will be created\n"
+                           "2: modifying existing worksheet? \n\t- Be advice that existing rule will be cleared make sure to have a backup beforehand\n"
+                           "Please enter a choice: "))
+        if choice == 1:
+            name = input("\nPlease enter the name for the json file \n\t** no need to include .json extension\nName: ")
+            write_column_rule("create", name)
+            break
+        elif choice == 2:
+            name = input("\nPlease enter the name of the json file \n\t** no need to include .json extension\nName: ")
+            write_column_rule("modify", name)
+            break
+        else:
+            print("\nPlease only select 1 or 2")
+    except ValueError:
+        print("Please enter digits only!!!")
 
 print("Sheet\'s column have been retrieved...")
-time.sleep(3)
 
-try:
-    with open("json/column_rules.json", "r") as file:
-        file.read()
-except FileNotFoundError:
-    print("column_rules.json file is being created...")
-    time.sleep(3)
-    open("json/column_rules.json", "x")
-finally:
-    with open("json/column_rules.json", "w") as file:
-        json.dump(headers_json, file, indent=4)
-
-try:
-    with open("json/data.json", "r") as file:
-        file.read()
-except FileNotFoundError:
-    open("json/data.json", "x")
-finally:
-    with open("json/data.json", "w") as file:
-        json.dump(rows, file, indent=2)
-    print("column_rules.json has been created, it is stored at the folder called json, please refer to readme.md to configure that json file")
+print("Please configure the rules to be applied for the column, then run the main.py to start the validation")
+# main()
